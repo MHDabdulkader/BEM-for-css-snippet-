c977e01c33465508edfbc05ece538b35c2606945
MHDabdulkader <81324227+MHDabdulkader@users.noreply.github.com>
2024-07-15 02:48:02 +0000

    New upadate

---
+++ Accordion_demo/script.js
@@ -0,0 +1,11 @@
+const title = document.querySelector('.js-title');
+const title2 = document.getElementsByClassName('.js-title');
+
+title.onclick = function(){
+    const copy_Open = document.querySelector("accordion__copy--open");
+    console.log(copy_Open);
+
+    
+}
+console.log(title);
+console.log(title.innerHTML);
